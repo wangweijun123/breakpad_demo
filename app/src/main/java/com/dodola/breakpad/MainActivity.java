5769a1ab8bb33d4eb3c6fd0a5a81107b051f5a1a
Your Name <you@example.com>
2021-09-14 01:00:44 +0800

    catch c/c++ exception

---
+++ app/src/main/java/com/dodola/breakpad/MainActivity.java
@@ -0,0 +1,90 @@
+package com.dodola.breakpad;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.sample.breakpad.BreakpadInit;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "dodoodla_crash";
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 100;
+    private File externalReportPath;
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("crash-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        } else {
+            initExternalReportPath();
+        }
+        findViewById(R.id.crash_btn)
+                .setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                initBreakPad();
+                                crash();
+                                // copy core dump to sdcard
+                            }
+                        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        initExternalReportPath();
+    }
+
+    private void initExternalReportPath() {
+        externalReportPath = new File(Environment.getExternalStorageDirectory(), "crashDump");
+        if (!externalReportPath.exists()) {
+            externalReportPath.mkdirs();
+        }
+
+        initBreakPad();
+    }
+
+    /**
+     * 一般来说，crash捕获初始化都会放到Application中，这里主要是为了大家有机会可以把崩溃文件输出到sdcard中
+     * 做进一步的分析
+     */
+    private void initBreakPad() {
+        if (externalReportPath == null) {
+            externalReportPath = new File(getFilesDir(), "crashDump");
+            if (!externalReportPath.exists()) {
+                externalReportPath.mkdirs();
+            }
+        }
+        Log.i(TAG, "init breadpad");
+        BreakpadInit.initBreakpad(externalReportPath.getAbsolutePath());
+    }
+
+    public native void crash();
+
+}
